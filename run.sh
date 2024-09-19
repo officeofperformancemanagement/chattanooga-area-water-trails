02420773eb2727d362d6d8d6728055325d1c4c28
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-09-19 18:31:46 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 curl --output data.kml "https://www.google.com/maps/d/kml?mid=1Asf30mVZiQzJYvGJUyh3t95lrC3PRua2&forcekml=1"
 
-ogr2ogr data.geojson "/vsicurl/https://www.google.com/maps/d/kml?mid=1Asf30mVZiQzJYvGJUyh3t95lrC3PRua2&forcekml=1"
+ogr2ogr data.geojson "/vsicurl/https://www.google.com/maps/d/kml?mid=1Asf30mVZiQzJYvGJUyh3t95lrC3PRua2&forcekml=1" -skipfailure
